2a81ce6021e5fe2c37ce9189a4fa5c9513d32edf
Samuel Chung <41113285+samchung95@users.noreply.github.com>
2024-11-03 22:08:53 +0800

    feat: sentiment analysis

---
+++ analytics/sentiment_analysis/readme.md
@@ -0,0 +1,22 @@
+# Requirements:
+- Python 3.11.10 (tested and working for sam, does not mean other versions will not work)
+    - pip install -r requirements.txt
+- CUDA (For faster inferance)
+- spaCy (https://spacy.io/usage)
+- Pytorch (https://pytorch.org/get-started/locally/)
+- Hugging face tiwtter roBERTa model (https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment/tree/main), download all files (except ipynb_checkpoints) and place in the same directory as the script under `cardiffnlp/twitter-roberta-base-sentiment`
+
+    ![alt text](image.png)
+
+# Usage:
+Refer to `sentiment_analysis.ipynb` for usage example
+```python
+from utils import get_sentiment
+
+df = pd.read_csv(df_path)
+
+df["sentiment"] = df.apply(lambda x: get_sentiment(x[target_column]), axis=1)
+df["negative"] = df["sentiment"].apply(lambda x: x["negative"])
+df["neutral"] = df["sentiment"].apply(lambda x: x["neutral"])
+df["positive"] = df["sentiment"].apply(lambda x: x["positive"])
+```
---
+++ analytics/sentiment_analysis/utils.py
@@ -0,0 +1,71 @@
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import numpy as np
+from scipy.special import softmax
+import re
+import spacy
+
+nlp = spacy.load("xx_ent_wiki_sm")
+
+
+
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+def preprocess_text(text):
+    """
+    Preprocess the input text using the provided spaCy model.
+
+    Args:
+        text (str): The input text to preprocess.
+        nlp_model: The preloaded spaCy language model.
+
+    Returns:
+        str: The preprocessed text.
+    """
+    # Process the text with the spaCy model
+    doc = nlp(text)
+    processed_text = doc.text.strip()
+    return processed_text
+
+def get_sentiment(text) -> dict:
+    try:
+        text = str(text)
+        text = preprocess_text(text)
+    
+    
+        if len(text) == 0:
+            return {
+                "positive": 0.0,
+                "neutral": 0.0,
+                "negative": 0.0
+            }
+    
+        encoded_input = tokenizer(text, return_tensors='pt')
+        output = model(**encoded_input)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+
+        ranking = np.argsort(scores)
+        ranking = ranking[::-1]
+
+        negative = scores[ranking[0]]
+        neutral = scores[ranking[1]]
+        positive = scores[ranking[2]]
+        
+        return {
+            "positive": positive,
+            "neutral": neutral,
+            "negative": negative
+        }
+        
+    except:
+        return {
+            "positive": 0.0,
+            "neutral": 0.0,
+            "negative": 0.0
+        }
+
+
+
